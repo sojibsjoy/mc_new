833ca8b61fa4ee3d0e897d374dd3b6b554e5098e
sojibsjoy <sojibsjoy336@gmail.com>
2021-01-19 08:53:26 +0600

    Full Recipe Updated...

--- lib/pages/view_recipe.dart
+++ lib/pages/view_recipe.dart
@@ -77,9 +77,145 @@ class _ViewRecipeState extends State<ViewRecipe> {
           .where((String key) => key.contains('.jpg'))
           .toList();
     } else if (widget.recipeNo == 7) {
-      widget.recipeDetails = await getFileData('assets/recipes/beafr.txt');
+      widget.recipeDetails = await getFileData('assets/recipes/borhani.txt');
       widget.images = manifestMap.keys
-          .where((String key) => key.contains('recipe_img/beafr/'))
+          .where((String key) => key.contains('recipe_img/borhani/'))
+          .where((String key) => key.contains('.jpg'))
+          .toList();
+    } else if (widget.recipeNo == 8) {
+      widget.recipeDetails = await getFileData('assets/recipes/chickenm.txt');
+      widget.images = manifestMap.keys
+          .where((String key) => key.contains('recipe_img/chickenm/'))
+          .where((String key) => key.contains('.jpg'))
+          .toList();
+    } else if (widget.recipeNo == 9) {
+      widget.recipeDetails = await getFileData('assets/recipes/coffee.txt');
+      widget.images = manifestMap.keys
+          .where((String key) => key.contains('recipe_img/coffee/'))
+          .where((String key) => key.contains('.jpg'))
+          .toList();
+    } else if (widget.recipeNo == 10) {
+      widget.recipeDetails = await getFileData('assets/recipes/donuts.txt');
+      widget.images = manifestMap.keys
+          .where((String key) => key.contains('recipe_img/donuts/'))
+          .where((String key) => key.contains('.jpg'))
+          .toList();
+    } else if (widget.recipeNo == 11) {
+      widget.recipeDetails = await getFileData('assets/recipes/friedrice.txt');
+      widget.images = manifestMap.keys
+          .where((String key) => key.contains('recipe_img/friedrice/'))
+          .where((String key) => key.contains('.jpg'))
+          .toList();
+    } else if (widget.recipeNo == 12) {
+      widget.recipeDetails = await getFileData('assets/recipes/chickenfry.txt');
+      widget.images = manifestMap.keys
+          .where((String key) => key.contains('recipe_img/chickenfry/'))
+          .where((String key) => key.contains('.jpg'))
+          .toList();
+    } else if (widget.recipeNo == 13) {
+      widget.recipeDetails = await getFileData('assets/recipes/chineseveg.txt');
+      widget.images = manifestMap.keys
+          .where((String key) => key.contains('recipe_img/chineseveg/'))
+          .where((String key) => key.contains('.jpg'))
+          .toList();
+    } else if (widget.recipeNo == 14) {
+      widget.recipeDetails = await getFileData('assets/recipes/custard.txt');
+      widget.images = manifestMap.keys
+          .where((String key) => key.contains('recipe_img/custard/'))
+          .where((String key) => key.contains('.jpg'))
+          .toList();
+    } else if (widget.recipeNo == 15) {
+      widget.recipeDetails = await getFileData('assets/recipes/pooding.txt');
+      widget.images = manifestMap.keys
+          .where((String key) => key.contains('recipe_img/pooding/'))
+          .where((String key) => key.contains('.jpg'))
+          .toList();
+    } else if (widget.recipeNo == 16) {
+      widget.recipeDetails =
+          await getFileData('assets/recipes/shorsheilish.txt');
+      widget.images = manifestMap.keys
+          .where((String key) => key.contains('recipe_img/shorsheilish/'))
+          .where((String key) => key.contains('.jpg'))
+          .toList();
+    } else if (widget.recipeNo == 17) {
+      widget.recipeDetails = await getFileData('assets/recipes/kalavuna.txt');
+      widget.images = manifestMap.keys
+          .where((String key) => key.contains('recipe_img/kalavuna/'))
+          .where((String key) => key.contains('.jpg'))
+          .toList();
+    } else if (widget.recipeNo == 18) {
+      widget.recipeDetails =
+          await getFileData('assets/recipes/beefshashlik.txt');
+      widget.images = manifestMap.keys
+          .where((String key) => key.contains('recipe_img/beefshashlik/'))
+          .where((String key) => key.contains('.jpg'))
+          .toList();
+    } else if (widget.recipeNo == 19) {
+      widget.recipeDetails = await getFileData('assets/recipes/chawmein.txt');
+      widget.images = manifestMap.keys
+          .where((String key) => key.contains('recipe_img/chawmein/'))
+          .where((String key) => key.contains('.jpg'))
+          .toList();
+    } else if (widget.recipeNo == 20) {
+      widget.recipeDetails =
+          await getFileData('assets/recipes/chickensharma.txt');
+      widget.images = manifestMap.keys
+          .where((String key) => key.contains('recipe_img/chickensharma/'))
+          .where((String key) => key.contains('.jpg'))
+          .toList();
+    } else if (widget.recipeNo == 21) {
+      widget.recipeDetails =
+          await getFileData('assets/recipes/chingrimalaikari.txt');
+      widget.images = manifestMap.keys
+          .where((String key) => key.contains('recipe_img/chingrimalaikari/'))
+          .where((String key) => key.contains('.jpg'))
+          .toList();
+    } else if (widget.recipeNo == 22) {
+      widget.recipeDetails = await getFileData('assets/recipes/chotpoti.txt');
+      widget.images = manifestMap.keys
+          .where((String key) => key.contains('recipe_img/chotpoti/'))
+          .where((String key) => key.contains('.jpg'))
+          .toList();
+    } else if (widget.recipeNo == 23) {
+      widget.recipeDetails = await getFileData('assets/recipes/cookies.txt');
+      widget.images = manifestMap.keys
+          .where((String key) => key.contains('recipe_img/cookies/'))
+          .where((String key) => key.contains('.jpg'))
+          .toList();
+    } else if (widget.recipeNo == 24) {
+      widget.recipeDetails = await getFileData('assets/recipes/eggchop.txt');
+      widget.images = manifestMap.keys
+          .where((String key) => key.contains('recipe_img/eggchop/'))
+          .where((String key) => key.contains('.jpg'))
+          .toList();
+    } else if (widget.recipeNo == 25) {
+      widget.recipeDetails = await getFileData('assets/recipes/fishcurry.txt');
+      widget.images = manifestMap.keys
+          .where((String key) => key.contains('recipe_img/fishcurry/'))
+          .where((String key) => key.contains('.jpg'))
+          .toList();
+    } else if (widget.recipeNo == 26) {
+      widget.recipeDetails = await getFileData('assets/recipes/halim.txt');
+      widget.images = manifestMap.keys
+          .where((String key) => key.contains('recipe_img/halim/'))
+          .where((String key) => key.contains('.jpg'))
+          .toList();
+    } else if (widget.recipeNo == 27) {
+      widget.recipeDetails = await getFileData('assets/recipes/jilapi.txt');
+      widget.images = manifestMap.keys
+          .where((String key) => key.contains('recipe_img/jilapi/'))
+          .where((String key) => key.contains('.jpg'))
+          .toList();
+    } else if (widget.recipeNo == 28) {
+      widget.recipeDetails = await getFileData('assets/recipes/masalatea.txt');
+      widget.images = manifestMap.keys
+          .where((String key) => key.contains('recipe_img/masalatea/'))
+          .where((String key) => key.contains('.jpg'))
+          .toList();
+    } else if (widget.recipeNo == 29) {
+      widget.recipeDetails = await getFileData('assets/recipes/redvelvet.txt');
+      widget.images = manifestMap.keys
+          .where((String key) => key.contains('recipe_img/redvelvet/'))
           .where((String key) => key.contains('.jpg'))
           .toList();
     }
